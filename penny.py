75d0d2b6f64ab1a1096560aa6fb35bccc28c611f
Ethan Puckett <ethanpuckett@dynamic-oit-vapornet100-10-9-68-138.princeton.edu>
2024-03-07 21:39:19 -0500

    start

--- penny.py
+++ penny.py
@@ -11,6 +11,14 @@ import flask
 
 app = flask.Flask(__name__, template_folder='.')
 
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+
+    html_code = flask.render_template('index.html')
+    response = flask.make_response(html_code)
+    return response
+
 #-----------------------------------------------------------------------
 
 
