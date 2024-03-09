ae5306c5c42bf2f72deec5662919593aabe23e1a
Ethan Puckett <ethanpuckett@Ethans-Laptop-2.local>
2024-03-09 01:03:10 -0500

    start

--- penny.py
+++ penny.py
@@ -20,7 +20,7 @@ def index():
     return response
 
 #-----------------------------------------------------------------------
-@app.route('/static/<path:filename>')
+@app.route('/<path:filename>')
 def serve_static(filename):
     if filename == 'css/card.css' or filename == 'js/card.js':
         return flask.send_from_directory('.', filename)
