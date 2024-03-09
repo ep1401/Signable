a4f577fc8e447288b4721393fb9b1262644cddc1
Ethan Puckett <ethanpuckett@Ethans-Laptop-2.local>
2024-03-09 11:00:15 -0500

    start

--- penny.py
+++ penny.py
@@ -14,19 +14,11 @@ app = flask.Flask(__name__, template_folder='.')
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-
     html_code = flask.render_template('index.html')
     response = flask.make_response(html_code)
     return response
 
 #-----------------------------------------------------------------------
-@app.route('/', methods=['GET'])
-@app.route('/card', methods=['GET'])
-def index():
-
-    html_code = flask.render_template('card.css')
-    response = flask.make_response(html_code)
-    return response
 
 
 #-----------------------------------------------------------------------
