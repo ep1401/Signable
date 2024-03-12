d82bb86cc63a0f831c4bc9db497716410d028980
Ethan Puckett <ethanpuckett@Ethans-Laptop-2.local>
2024-03-11 23:57:28 -0400

    start

--- penny.py
+++ penny.py
@@ -1,37 +1,21 @@
 #!/usr/bin/env python
 
-#-----------------------------------------------------------------------
-# penny.py
-# Author: Bob Dondero
-#-----------------------------------------------------------------------
-
 import flask
 
-#-----------------------------------------------------------------------
-
-app = flask.Flask(__name__, template_folder='.', static_url_path='/static', static_folder='static')
-
+app = flask.Flask(__name__, template_folder='.')
 
 @app.route('/', methods=['GET'])
-@app.route('/lessons', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def index():
     html_code = flask.render_template('index.html')
     response = flask.make_response(html_code)
     return response
 
-
-
-#-----------------------------------------------------------------------
-
-@app.route('/index', methods=['GET'])
+@app.route('/lessons', methods=['GET'])
 def lessons():
     html_code = flask.render_template('lessons.html')
     response = flask.make_response(html_code)
     return response
 
-#-----------------------------------------------------------------------
-
-
-
-#-----------------------------------------------------------------------
-
+if __name__ == '__main__':
+    app.run(debug=True)
