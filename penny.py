bdaa144d851239d2b5c600dda7842eaf388bb4a8
Ethan Puckett <ethanpuckett@Ethans-Laptop-2.local>
2024-03-12 03:20:41 -0400

    start

--- penny.py
+++ penny.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 
 import flask
+from flask import request
 
 app = flask.Flask(__name__, template_folder='.')
+
+course_lessonsnum = {
+    'ASL101': 14,
+    'ASL102': 11,
+    'ASL105': 8,
+    'ASL107': 9
+}
+
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
@@ -13,7 +22,23 @@ def index():
 
 @app.route('/lessons', methods=['GET'])
 def lessons():
-    html_code = flask.render_template('lessons.html')
+    input = request.args.get('course', default=None)
+    values = input.split()
+    
+    # Your existing code for rendering the template
+    html_code = flask.render_template('lessons.html', course=values[0], 
+        lesson_num = values[1])
+    response = flask.make_response(html_code)
+    return response
+
+
+@app.route('/selectlessons', methods=['GET'])
+def selectlessons():
+    course = request.args.get('course', default=None)
+    
+    # Your existing code for rendering the template
+    html_code = flask.render_template('selectlessons.html', course=course,
+        lesson_num = course_lessonsnum[course])
     response = flask.make_response(html_code)
     return response
 
