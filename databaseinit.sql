212329ebfb14203a584be4818d2ccff407b3808c
VishvaIlavelan <vi6908@princeton.edu>
2024-03-21 07:07:11 -0700

    done

---
+++ databaseinit.sql
@@ -0,0 +1,11 @@
+-- lessonnumber 
+CREATE TABLE classes (courseid INTEGER, lessonid INTEGER, lessonname TEXT)
+-- question: cardid unique among all cards? -> might be good to have it unique so we can easily implement starred flashcards
+CREATE TABLE flashcards (courseid INTEGER, lessonid INTEGER, cardid INTEGER,
+    videolink TEXT, translation TEXT, memorytip TEXT, speech TEXT, sentence TEXT)
+
+CREATE TABLE studentusers(netid TEXT, firstname TEXT, lastname TEXT)
+
+CREATE TABLE starredflashcards(netid TEXT, cardid INTEGER)
+
+CREATE TABLE admin(adminid INTEGER, firstname text, lastname TEXT)
