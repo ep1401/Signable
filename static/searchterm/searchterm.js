26ee0f215deb68fa17e65eb9885fdcddd2c31282
Ethan Puckett <ethanpuckett@Ethans-Laptop-2.local>
2024-05-04 16:19:48 -0400

    fixed lesson table

--- static/searchterm/searchterm.js
+++ static/searchterm/searchterm.js
@@ -7,7 +7,6 @@ function handleResponse(data) {
 function handleError(xhr, textStatus, errorThrown) {
     console.log(xhr)
     if (xhr.responseText && xhr.responseText.startsWith("<")) {
-        // Display HTML error message
         alert('This website is using a security service to protect itself from online attacks. The action you just performed triggered the security solution. There are several actions that could trigger this block including submitting a certain word or phrase, a SQL command or malformed data.');
     }
    else {
